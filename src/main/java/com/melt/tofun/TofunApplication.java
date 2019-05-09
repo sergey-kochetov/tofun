acd6cd1ed2c052acf65573d4e8e6a245b80d2448
sergey <git.kochetov@gmail.com>
2019-05-09 11:11:34 +0200

    init commit

---
+++ src/main/java/com/melt/tofun/TofunApplication.java
@@ -0,0 +1,13 @@
+package com.melt.tofun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TofunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TofunApplication.class, args);
+	}
+
+}
